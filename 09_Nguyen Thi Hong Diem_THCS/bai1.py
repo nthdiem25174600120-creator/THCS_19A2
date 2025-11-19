a43539b0d3819ba8274fe253a15cf9acc9afdc95
nthdiem25174600120-creator <nthdiem.25174600120@sv.uneti.edu.vn>
2025-11-19 15:53:44 +0700

    Delete 09_Nguyen Thi Hong Diem_THCS/bai1.py

--- 09_Nguyen Thi Hong Diem_THCS/bai1.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Nhập một số: "))
-i = 0
-while i * i <= n:
-    if i * i == n:
-        print(n, "là số chính phương.")
-        break
-    i += 1
-else:
-    print(n, "không phải số chính phương.")
